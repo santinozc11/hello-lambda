3431388f2a9d86bc67ff9a35d82ee8b8c05ce17f
Santino Zahir Chamia <95001@sistemas.frc.utn.edu.ar>
2025-10-22 17:31:29 -0300

    commit pipeline

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 ﻿exports.handler = async () => {
-  return { statusCode: 200, body: JSON.stringify({ msg: "Hello v2 desde pipeline" }) };
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ msg: "Hello desde pipeline" }),
+  };
 };
